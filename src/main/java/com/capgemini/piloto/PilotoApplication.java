97b8ced7a68a5b9fafe2b57da04822627745cea4
karolmc02 <karolmc02@gmail.com>
2018-01-23 09:42:04 +0100

    Version inicial limpia

---
+++ src/main/java/com/capgemini/piloto/PilotoApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.piloto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotoApplication.class, args);
+	}
+}
