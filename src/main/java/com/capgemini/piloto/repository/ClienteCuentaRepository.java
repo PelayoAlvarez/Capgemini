823a9fbd22a71306028f6520c0463bbb5eb9b721
CORP\ogomezte <olivergmez11@gmail.com>
2018-01-25 10:26:56 +0100

    Cambios en el repositorio cliente cuenta

--- src/main/java/com/capgemini/piloto/repository/ClienteCuentaRepository.java
+++ src/main/java/com/capgemini/piloto/repository/ClienteCuentaRepository.java
@@ -1,16 +1,10 @@
 package com.capgemini.piloto.repository;
 
 
-<<<<<<< HEAD
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.capgemini.piloto.model.types.ClienteCuentaKey;
-
-=======
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.capgemini.piloto.model.types.ClienteCuentaKey;
 
->>>>>>> d9115fd17d5f43e12fbe35c7db6a1a3a385aca25
 public interface ClienteCuentaRepository  extends JpaRepository<ClienteCuentaKey,Long>{
 
 }
