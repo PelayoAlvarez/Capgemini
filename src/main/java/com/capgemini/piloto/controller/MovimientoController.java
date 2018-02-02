f0f2d5e47f5ef51c5c028c1f631264f9eb081b76
PelayoAlvarez <pelayoalvarez11@gmail.com>
2018-02-02 13:44:00 +0100

    Anotacion CrossOrigin en MovimientoController

--- src/main/java/com/capgemini/piloto/controller/MovimientoController.java
+++ src/main/java/com/capgemini/piloto/controller/MovimientoController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,6 +41,7 @@ import com.capgemini.piloto.util.validator.ImporteValidator;
 
 @RestController
 @RequestMapping(path = "/movimiento")
+@CrossOrigin
 public class MovimientoController {
 
 	private static final String NOT_FOUND = "The requested transaction was not found";
