f4e90fc6973c9fb3b70bb8db4175f5028c1e37f7
PelayoAlvarez <pelayoalvarez11@gmail.com>
2018-02-08 09:11:25 +0100

    Retoques en Movimientos.

--- src/main/java/com/capgemini/piloto/repository/MovimientoRepository.java
+++ src/main/java/com/capgemini/piloto/repository/MovimientoRepository.java
@@ -3,6 +3,7 @@ package com.capgemini.piloto.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.capgemini.piloto.model.Cuenta;
 import com.capgemini.piloto.model.Movimiento;
@@ -10,6 +11,8 @@ import com.capgemini.piloto.model.Movimiento;
 public interface MovimientoRepository extends JpaRepository<Movimiento, Long> {
 
 	Movimiento findMovimientoById(Long id);
-
+	
+	@Query("SELECT m FROM Movimiento m WHERE m.cuentaAsociada=?1 "
+			+ "AND m.mCAHabilitado=true ORDER BY m.fechahora DESC")
 	List<Movimiento> findByCuentaAsociada(Cuenta cuenta);
 }
